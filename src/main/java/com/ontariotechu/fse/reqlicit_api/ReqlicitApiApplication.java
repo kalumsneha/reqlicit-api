4933c9ff421ee62e7f48ef9d0eafd5bfb7c29972
mikalumfernando <kalumsneha@yahoo.com>
2024-11-10 05:43:22 +0100

    Initial Commit

---
+++ src/main/java/com/ontariotechu/fse/reqlicit_api/ReqlicitApiApplication.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.fse.reqlicit_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReqlicitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReqlicitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ontariotechu/fse/reqlicit_api/ReqlicitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.fse.reqlicit_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReqlicitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
